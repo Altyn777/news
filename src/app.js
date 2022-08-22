1bf700a56a5a0a05f90d072675e646b0ad1db94b
Altyn777 <darzeeia@gmail.com>
2022-08-22 22:02:41 +0300

    routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 // import "./app.css";
+import { Link, Outlet } from "react-router-dom";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="news">News</Link>
+        <Link to="profile">Profile</Link>
+        <Link to="login">Login</Link>
+      </nav>
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./app";
 
@@ -9,6 +10,15 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<>Home</>} />
+          <Route path="login" element={<>login</>} />
+          <Route path="news" element={<>news</>} />
+          <Route path="profile" element={<>profile</>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
